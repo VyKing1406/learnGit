a7bec7704f4227421b393d5e762ee92914a31de3
VyKing1406 <vuong.le1406@hcmut.edu.vn>
2023-06-15 17:23:55 +0700

    tui sua lai cho dev 1

--- code.js
+++ code.js
@@ -1,4 +1,5 @@
 const t = "tui testcode"
 const dev2 = "tui code them dev2 ne"
 const dev22 = "Tui code cn 2 ne"
-const dev1 = "Tui code cn 1 ne"
+const dev1 = "Tui code cn 1 ne tui o day lam them nhieu thu ne"
+
