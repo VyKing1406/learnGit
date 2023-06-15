43bacc4fb3e63733063718c8397e736e607f0b1f
VyKing1406 <vuong.le1406@hcmut.edu.vn>
2023-06-15 17:26:24 +0700

    aha

--- code.js
+++ code.js
@@ -3,3 +3,5 @@ const dev2 = "tui code them dev2 ne"
 const dev22 = "Tui code cn 2 ne"
 const dev1 = "Tui code cn 1 ne tui o day lam them nhieu thu ne"
 const dev222 = "ahjhj chuc nang tui code ne tui tui lam ne"
+const dev2222 = "ahjhj tui lam them nua na"
+
